7deb9f304c1e313407fdc28261544aaf98056c3e
vasharma05 <sharmava05@gmail.com>
2020-01-13 22:13:09 +0530

    Updated Models

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.core.reverse_lazy
+
+
+from . import forms
 
 # Create your views here.
--- social_media_clone/settings.py
+++ social_media_clone/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'social_media_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
